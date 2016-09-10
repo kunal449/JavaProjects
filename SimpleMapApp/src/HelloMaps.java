45f906efad6748c932feec4d7daf4cd87e21ae06
kunal449 <kunal44mhl@gmail.com>
2016-09-10 16:09:08 +0530

    Adding comments

--- SimpleMapApp/src/HelloMaps.java
+++ SimpleMapApp/src/HelloMaps.java
@@ -4,18 +4,27 @@ import de.fhpotsdam.unfolding.providers.Google;
 import de.fhpotsdam.unfolding.utils.MapUtils;
 import processing.core.PApplet;
 
+/***
+ * A simple Applet to draw a map, using UnfoldingMap library.
+ * @author Kunal
+ *	Date : 10/08/2016
+ */
 public class HelloMaps extends PApplet{
 	UnfoldingMap map;
 	@Override
 	public void setup() {
+		//Set the size of window
 		size(1000,800);
 		
 		map = new UnfoldingMap(this, new Google.GoogleTerrainProvider());
+		//Define the default location and zoom-in level when the map loads.
 		map.zoomAndPanTo(15, new Location(20.0471, 74.4820));
+		//This line makes maps interactive.
 		MapUtils.createDefaultEventDispatcher(this, map);
 	}
 	@Override
 	public void draw() {
+		//Set the background color 
 		background(0);
 		map.draw();
 	}
